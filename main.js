ffcda0d8f0dc26026f352bdbbb8bf64b3870cb35
mary3210 <maryrodri3210@gmail.com>
2022-11-15 15:20:48 -0600

    The whoIsWinner function can now display the winner

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-const gameOn = true;
 const userCardOne = document.querySelector("#userCardOne")
 const userCardTwo = document.querySelector("#userCardTwo")
 const dealerCardOne = document.querySelector("#dealerCardOne")
@@ -16,6 +15,7 @@ function shuffleCards(){
 
 //Events of the game
 function gamePlay(){
+    const gameOn = true;
     userCardOne.innerHTML = shuffleCards()
     userCardTwo.innerHTML = shuffleCards()
     dealerCardOne.innerHTML = shuffleCards()
@@ -27,18 +27,22 @@ function gamePlay(){
 //checking if users cards is less than, equal, or greater than 21
 function checkingValues(){
     let sum = 0
-    for( i = 0; i < cardList.length; i++){
+    sum += parseInt(cardList[0])
+    console.log(sum)
+    for( i = 1; i < cardList.length; i++){
         sum+= parseInt(cardList[i])
         console.log(sum)
     }
     if(sum < 21){
       selectHitBtn()
+      
     }
     else if(sum == 21){
         console.log("You win")
     }
-    else{ console.log("you lost")}
-    
+    else{ 
+        console.log("you lost")}
+
 }
 //adds another card on the table
 function selectHitBtn(){
@@ -46,29 +50,68 @@ function selectHitBtn(){
     userCardsDiv.appendChild(newCard)
     newCard.innerHTML = shuffleCards()
     cardList.push(newCard.innerHTML)
+    
 }
 //the dealers option to hit or stay
 function dealersTurn(){
     dealerCardTwo.style.display = "unset"
-    let dealersCardList = [dealerCardOne.innerHTML, dealerCardTwo.innerHTML ]
+    
+    console.log(dealersCardList)
     let sum = 0
-    for( i = 0; i < dealersCardList.length; i++){
+        sum += parseInt(dealersCardList[0])
+    for( i = 1; i < dealersCardList.length; i++){
         sum+= parseInt(dealersCardList[i])
         console.log(sum)
-    }
+    
     if(sum < 17){
         const dealersNewCard = document.createElement("p")
         dealersCardsDiv.appendChild(dealersNewCard)
         dealersNewCard.innerHTML = shuffleCards()
         dealersCardList.push(dealersNewCard.innerHTML)
+        console.log(dealersCardList)
+    }
+    else if(sum == 21){
+        console.log("blackjack")
+    }
+    else if(sum > 21){
+        console.log("Dealer Busted")
+        break
     }
     else{
-        console.log("woo")
+        console.log("this is where we are at " + sum)
+    }
+    }
+    whoIsWinner()
+}
+
+function whoIsWinner(){
+    let userSum = 0
+    let dealerSum = 0
+    for (i = 0; i <cardList.length; i++){
+        userSum+= parseInt(cardList[i])
+    }
+    for (i = 0; i <dealersCardList.length; i++){
+        dealerSum+= parseInt(dealersCardList[i])
+}
+    if (userSum > dealerSum && userSum < 21)
+    console.log("yay you win!")
+    else if( dealerSum > userSum && dealerSum < 21){
+        console.log("you lose")
+    }
+    else if(userSum == dealerSum){
+        console.log("it's a tie")
+    }
+    else if(userSum >21){
+        console.log("Dealer Wins")
+    }
+    else if(dealerSum >21){
+        console.log("User wins")
+
     }
 }
 gamePlay()
 const cardList = [userCardOne.innerHTML, userCardTwo.innerHTML]
-
+const dealersCardList = [dealerCardOne.innerHTML, dealerCardTwo.innerHTML]
 
 
 hitBtn.addEventListener("click", checkingValues)
